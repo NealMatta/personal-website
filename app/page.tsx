1acd68366b3f6795a1c8e13def5ff3afb610eabd
Neal Matta <nlsima97@gmail.com>
2024-11-15 00:36:49 -0600

    Got the cards to be responsive as the screen changes size

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import About from '@/src/components/About/About';
 export default function Home() {
   return (
     <>
-      <h1>Hello world!</h1>
+      <div className="flex flex-col md:flex-row md:gap-4">
+        <div className="w-full md:w-2/3">
+          <About />
+        </div>
+        <div className="w-full md:w-1/3">
+          <About />
+          <About />
+        </div>
+      </div>
     </>
   );
 }
