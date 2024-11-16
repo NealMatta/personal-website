13afeb155dc48c7cf8db2c2f702121c2ef3380e0
Neal Matta <nlsima97@gmail.com>
2024-11-16 00:36:46 -0600

    Starting to set up the intro. Moving into the about afterwards

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import About from '@/src/components/About/About';
 export default function Home() {
   return (
-    <>
+    <div className="container mt-20">
+      <div className="mb-4">
+        <h1 className="text-3xl">Good Afternoon</h1>
+        {/* Morning, Afternoon, and Evening changes depending on time */}
+        <h3>It&apos;s November 16 2024</h3>
+      </div>
       <div className="flex flex-col md:flex-row md:gap-4">
         <div className="w-full md:w-2/3">
           <About />
@@ -11,6 +16,6 @@ export default function Home() {
           <About />
         </div>
       </div>
-    </>
+    </div>
   );
 }
