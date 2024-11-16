8189a8503c8685b90eaf64cf9d777fa29be0753b
Neal Matta <nlsima97@gmail.com>
2024-11-16 01:11:29 -0600

    I got the general layout done. I need to figure out the social links though

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,30 @@
 import About from '@/src/components/About/About';
+import BasicCard from '@/src/components/card/BasicCard';
+
 export default function Home() {
   return (
-    <div className="container mt-20">
+    <div className="container mt-5 md:mt-20">
       <div className="mb-4">
         <h1 className="text-3xl">Good Afternoon</h1>
         {/* Morning, Afternoon, and Evening changes depending on time */}
         <h3>It&apos;s November 16 2024</h3>
       </div>
-      <div className="flex flex-col md:flex-row md:gap-4">
-        <div className="w-full md:w-2/3">
+
+      <div className="flex">
+        <div className="w-full">
           <About />
         </div>
+      </div>
+
+      <div className="flex flex-col md:flex-row md:gap-4">
+        <div className="w-full md:w-2/3">
+          <BasicCard>Projects</BasicCard>
+          <BasicCard>Submit Feedback</BasicCard>
+        </div>
         <div className="w-full md:w-1/3">
-          <About />
-          <About />
+          <BasicCard>Weather in Chicago</BasicCard>
+          <BasicCard>Resume Creator</BasicCard>
+          <BasicCard>Last Listened To</BasicCard>
         </div>
       </div>
     </div>
--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,5 +1,40 @@
 import BasicCard from '../card/BasicCard';
+import SocialLinks from './SocialLinks';
+// import Image from 'next/image';
 
 export default function About() {
-  return <BasicCard>Test</BasicCard>;
+  return (
+    <BasicCard>
+      <div className="flex gap-2">
+        <div className="w-1/4 bg-red-600">
+          <div className="flex h-full items-center justify-center ">
+            <p>Image Placehold</p>
+          </div>
+          {/* <Image
+            className=""
+            src="/next.svg"
+            alt="Next.js logo"
+            width={50}
+            height={50}
+            priority
+          /> */}
+        </div>
+        <div className="w-3/4">
+          <div className="flex flex-col">
+            <div className="flex gap-4 items-center ">
+              <h1 className="font-bold text-3xl">About Me </h1>
+              <SocialLinks />
+            </div>
+            <div>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+              vel arcu sit amet ipsum tincidunt mollis quis sed enim. Vivamus
+              vitae dapibus massa. Aenean laoreet ac velit a lacinia. Aliquam
+              malesuada arcu lacus, id dignissim eros tempus ut. Cras mattis
+              sollicitudin diam, sit amet eleifend justo egestas
+            </div>
+          </div>
+        </div>
+      </div>
+    </BasicCard>
+  );
 }
