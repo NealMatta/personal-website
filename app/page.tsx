22ebf1f2d4747b2ba474e920b491131145639965
Neal Matta <nlsima97@gmail.com>
2024-11-16 01:37:06 -0600

    Finished about me layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <div className="container mt-5 md:mt-20">
       <div className="mb-4">
-        <h1 className="text-3xl">Good Afternoon</h1>
+        <h1 className="text-3xl font-bold">Good Afternoon</h1>
         {/* Morning, Afternoon, and Evening changes depending on time */}
-        <h3>It&apos;s November 16 2024</h3>
+        <h3 className="text-xl ">It&apos;s November 16 2024</h3>
       </div>
 
       <div className="flex">
--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,16 +1,20 @@
 import BasicCard from '../card/BasicCard';
 import SocialLinks from './SocialLinks';
 // import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 export default function About() {
   return (
     <BasicCard>
-      <div className="flex gap-2">
-        <div className="w-1/4 bg-red-600">
+      <div className="flex flex-col md:flex-row gap-4 md:gap-2">
+        <div className="w-full md:w-1/4 ">
           <div className="flex h-full items-center justify-center ">
-            <p>Image Placehold</p>
-          </div>
-          {/* <Image
+            <div className="flex space-x-2">
+              <div className="relative flex h-24 w-24 items-center justify-center rounded-full border-4 border-slate-500 bg-red-400 ">
+                <FontAwesomeIcon icon={faUser} className="w-12" />
+              </div>
+              {/* <Image
             className=""
             src="/next.svg"
             alt="Next.js logo"
@@ -18,14 +22,16 @@ export default function About() {
             height={50}
             priority
           /> */}
+            </div>
+          </div>
         </div>
-        <div className="w-3/4">
+        <div className="w-full md:w-3/4">
           <div className="flex flex-col">
             <div className="flex gap-4 items-center ">
               <h1 className="font-bold text-3xl">About Me </h1>
               <SocialLinks />
             </div>
-            <div>
+            <div className="text-sm">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
               vel arcu sit amet ipsum tincidunt mollis quis sed enim. Vivamus
               vitae dapibus massa. Aenean laoreet ac velit a lacinia. Aliquam
