3490ccde4e8a643af8c819f07f9558b0855af911
Neal Matta <nlsima97@gmail.com>
2024-11-17 22:38:56 -0600

    Making small tweaks to the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <NavBar />
-        <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 xl:px-16">
+        <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-16 ">
           {children}
         </div>
       </body>
--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function NavBar() {
   return (
     <nav className="bg-slate-900">
-      <div className="max-w-4xl mx-auto p-2 sm:px-6 lg:px-8 xl:px-16">
+      <div className="max-w-5xl mx-auto p-2 sm:px-6 lg:px-16">
         <div className="flex justify-between">
           <div>
             <Link
