68ec7c94e93961e97501d9a4ba523eb8f5cdcbe6
Neal Matta <nlsima97@gmail.com>
2024-11-18 22:49:07 -0600

    I used grid instead of just flexto accomplish the 3 projects in large screens, two rpojects for medium screens, and one column for small screens

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PageHeader from '@/src/components/PageHeader/PageHeader';
+import mockProjectDetails from '@/public/dummyData/PROJECTS.json';
+import ClientSideProjects from '@/src/components/Projects/ClientSideProjects';
 
 const Projects = () => {
   return (
@@ -9,6 +11,11 @@ const Projects = () => {
         subHeader="A glimpse into my passions and progress — from coding and creative
           pursuits to personal growth and volleyball goals."
       />
+
+      <div className="grid gap-4 my-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+        {/* Pass mock project details to the client-side component */}
+        <ClientSideProjects projects={mockProjectDetails} />
+      </div>
     </>
   );
 };
