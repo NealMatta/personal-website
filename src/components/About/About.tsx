7e08c9270eaf0b473e41e4df70e874b9da4cde73
Neal Matta <nlsima97@gmail.com>
2024-11-24 18:54:36 -0600

    Finished the About Me section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,8 +1,6 @@
 import BarebonesCard from '../cards/BarebonesCard';
 import SocialLinks from './SocialLinks';
-// import Image from 'next/image';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import Image from 'next/image';
 
 export default function About() {
   return (
@@ -10,23 +8,33 @@ export default function About() {
       <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
         {/* Photo */}
         <div className="col-span-1 flex items-center justify-center">
-          <div className="relative flex h-32 w-32 items-center justify-center rounded border-4 border-slate-500 bg-red-400">
-            <FontAwesomeIcon icon={faUser} className="text-5xl" />
+          <div className="relative flex items-center justify-center">
+            <Image
+              src="/images/profile-picture.jpg"
+              alt="Profile Picture"
+              width={128}
+              height={128}
+              quality={100}
+              className="rounded border-4 border-slate-900"
+            />
           </div>
         </div>
 
         {/* Content */}
-        <div className="col-span-1 md:col-span-3 flex flex-col">
+        <div className="col-span-1 md:col-span-3 flex flex-col gap-2">
           <div className="flex flex-col lg:flex-row gap-4 items-center">
             <h1 className="font-bold text-4xl">About Me</h1>
             <SocialLinks />
           </div>
           <div className="text-md">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-            vel arcu sit amet ipsum tincidunt mollis quis sed enim. Vivamus
-            vitae dapibus massa. Aenean laoreet ac velit a lacinia. Aliquam
-            malesuada arcu lacus, id dignissim eros tempus ut. Cras mattis
-            sollicitudin diam, sit amet eleifend justo egestas.
+            Hey, I&apos;’m Neal! I&apos;m a tech enthusiast and serial hobbyist
+            working as an Enterprise Architect at Workday. I thrive on solving
+            problems, organizing chaos, and pushing my aging body to the limits
+            by playing too much volleyball. Outside of work, I&apos;m an avid
+            volleyball player, a (more recently) adventurous foodie, and someone
+            who finds joy in learning and creating. The purpose of this site is
+            to help me brush up on my coding skills and bring even more
+            organization to my life. Enjoy!
           </div>
         </div>
       </div>
