694f524abed63d5b46ee96b2edb7741eb1276500
Neal Matta <nlsima97@gmail.com>
2024-12-10 19:50:35 -0600

    typescript is validated throughout everywhere. First going to make the spofity look clean on the home page. Then need to add comments everywhere

--- app/api/spotify/recentSong/route.ts
+++ app/api/spotify/recentSong/route.ts
@@ -15,11 +15,15 @@ export async function GET() {
       status: 200,
       headers: { 'Content-Type': 'application/json' },
     });
-  } catch (error) {
+  } catch (error: unknown) {
+    // Narrow the type of the error
+    const errorMessage =
+      error instanceof Error ? error.message : 'An unknown error occurred';
+
     return new Response(
       JSON.stringify({
         error: 'Failed to fetch recent track',
-        details: error.message,
+        details: errorMessage,
       }),
       {
         status: 500,
