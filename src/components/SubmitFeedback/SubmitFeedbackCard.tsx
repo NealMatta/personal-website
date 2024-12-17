6f18c939f10bbc261274e82b3580507ba94427b0
Neal Matta <nlsima97@gmail.com>
2024-12-16 20:01:17 -0600

    Created the feedback form in Notion and linked it via a button for now

--- src/components/SubmitFeedback/SubmitFeedbackCard.tsx
+++ src/components/SubmitFeedback/SubmitFeedbackCard.tsx
@@ -3,7 +3,7 @@ import PrimaryButton from '../UI/PrimaryButton';
 
 export default function WebsiteStatus() {
   return (
-    <BarebonesCard title={'Submit Feedback'} wip={true}>
+    <BarebonesCard title={'Submit Feedback'}>
       <div className="flex flex-col gap-4">
         <div className="text-center">
           <p className="text-gray-500">I&apos;d love to hear what you think!</p>
@@ -11,7 +11,7 @@ export default function WebsiteStatus() {
 
         <div className="text-center">
           <PrimaryButton
-            linkTo="#"
+            linkTo="https://nealmatta.notion.site/15fb9bd7fc6680b7b218cb405427d385?pvs=105"
             aria-label="Submit Feedback"
             className="inline-flex items-center space-x-2"
           >
