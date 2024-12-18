2729a723f9536266009373d100b80e576726d6f7
Neal Matta <nlsima97@gmail.com>
2024-12-17 19:20:18 -0600

    updated the navbar to get this cool backdrop blur look as it scrolls down. Need to make it mobile friendly and add the similar nav bar structure from https://jackwhiting.co.uk/

--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -1,32 +1,59 @@
+'use client'; // For client-side interactivity
+
 import Link from 'next/link';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function NavBar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // Check if user has scrolled more than 50px
+      setIsScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <nav className="bg-slate-900">
-      <div className="max-w-7xl mx-auto p-2 sm:px-6 lg:px-16">
-        <div className="flex justify-between">
+    <nav
+      className={`sticky top-0 z-50 px-4 py-4 transition-all duration-300 ${
+        isScrolled ? 'backdrop-blur-sm' : 'bg-transparent'
+      }`}
+    >
+      <div className="max-w-7xl mx-auto sm:px-6 lg:px-16">
+        <div className="container flex justify-between items-center ">
+          {/* Logo Section */}
           <div>
             <Link
               href="/"
-              className="text-white uppercase tracking-widest font-bold "
+              className="text-slate-900 uppercase tracking-widest font-bold"
             >
               Neal Matta
             </Link>
           </div>
-          <div className="flex space-x-4">
-            <Link href="/projects" className="text-white">
-              Projects
-            </Link>
-            {/* <Link href="#" className="text-white">
-              Playground
-            </Link> */}
-            {/* <Link href="#" className="text-white">
-              Cookbook
-            </Link> */}
+
+          {/* Navigation Links */}
+          <div className="flex space-x-8">
+            <NavLink href="#" label="Projects" />
+            <NavLink href="#" label="Playground" />
+            <NavLink href="#" label="Cookbook" />
+            <NavLink href="#" label="Articles" />
           </div>
         </div>
       </div>
     </nav>
   );
 }
+
+/* Reusable NavLink Component */
+const NavLink = ({ href, label }: { href: string; label: string }) => (
+  <Link
+    href={href}
+    className="text-gray-500 hover:text-gray-700 transition-colors duration-300"
+  >
+    {label}
+  </Link>
+);
