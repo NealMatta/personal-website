3e0088c4cb0f405a4eabebe56f83f58c70afe5b9
Neal Matta <nlsima97@gmail.com>
2024-12-19 23:03:22 -0600

    Added the tags to the project on the projects page

--- src/components/Projects/Cards/ProjectCard.tsx
+++ src/components/Projects/Cards/ProjectCard.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
 // import Image from 'next/image';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { ProjectCardProps } from '@/src/types';
 
 export default function ProjectCard({ project }: ProjectCardProps) {
   return (
     <div
       className={
-        'flex flex-col p-2 border-2 rounded shadow-sm bg-white hover:opacity-50 transition'
+        'flex flex-col p-2 border-2 rounded shadow-md bg-white hover:opacity-50 transition'
       }
     >
       {/* Image would go here */}
-      <div className="flex h-48 w-full items-center justify-center rounded bg-red-400 ">
-        <FontAwesomeIcon icon={faUser} className="fa-icon" />
+      <div className="flex h-60 w-full items-center justify-center rounded bg-red-400 ">
+        {/* <FontAwesomeIcon icon={faUser} className="fa-icon" /> */}
       </div>
       <div className="pt-3">
-        <h1 className="font-bold text-xl">{project.title}</h1>
-        <p className="text-sm">{project.description}</p>
+        <h1 className="font-bold text-3xl mb-1.5">{project.title}</h1>
+        <div className="flex space-x-3">
+          {project.tags?.map((tag, index) => (
+            <p
+              key={index}
+              className="border border-foreground px-2 py-1 rounded-md uppercase text-xs tracking-wider italic mb-3"
+            >
+              {tag}
+            </p>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/types/projects.ts
+++ src/types/projects.ts
@@ -1,7 +1,18 @@
+interface ProjectInfo {
+  order: number;
+  id: number;
+  title: string;
+  detail: string;
+}
+
 export interface Project {
   id: number;
   title: string;
+  tags: string[];
+  featured: boolean;
   description: string;
+  details: ProjectInfo[];
+  images: string[];
 }
 
 export interface ProjectCardProps {
