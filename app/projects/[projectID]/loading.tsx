a2e48d20b21b85c83facb6ce12fb52c09705494a
Neal Matta <nlsima97@gmail.com>
2024-12-23 20:12:40 -0600

    Got the skeleton loader for the individual projects page

--- app/projects/[projectID]/loading.tsx
+++ app/projects/[projectID]/loading.tsx
@@ -1,3 +1,35 @@
 export default function Loading() {
-  return 'loading...';
+  return (
+    <div className="animate-pulse">
+      {/* Header Section */}
+      <div className="h-8 w-2/3 bg-gray-300 rounded mt-4"></div>
+      <div className="mt-2">
+        <div className="flex flex-wrap gap-3">
+          <div className="h-6 w-20 bg-gray-300 rounded"></div>
+          <div className="h-6 w-24 bg-gray-300 rounded"></div>
+          <div className="h-6 w-16 bg-gray-300 rounded"></div>
+        </div>
+      </div>
+
+      {/* Content Section */}
+      <div className="flex flex-col md:flex-row gap-8 mt-4">
+        {/* Details */}
+        <div className="flex-1 space-y-4">
+          {[1, 2, 3].map((_, index) => (
+            <div key={index} className="h-24 bg-gray-300 rounded-lg"></div>
+          ))}
+        </div>
+
+        {/* Images */}
+        <div className="flex-1 space-y-4">
+          {[1, 2].map((_, index) => (
+            <div
+              key={index}
+              className="w-full h-48 bg-gray-300 rounded-md"
+            ></div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
