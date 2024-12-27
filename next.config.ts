c7e5b85d631fb57d8f5dd26965189b5cdcfb40f3
Neal Matta <nlsima97@gmail.com>
2024-12-27 00:36:06 -0600

    Getting started with the playground

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,17 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['i.scdn.co', 'hpwskllhfbfuhsnysziq.supabase.co'], // Add the domain for Spotify's image host
+    // domains: ['i.scdn.co', 'hpwskllhfbfuhsnysziq.supabase.co'], // Add the domain for Spotify's image host
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.scdn.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'hpwskllhfbfuhsnysziq.supabase.co',
+      },
+    ],
   },
   /* other config options here */
 };
