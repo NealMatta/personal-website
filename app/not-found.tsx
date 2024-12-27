bc5bb8210c2bc2c982db4daea201e32a58380726
Neal Matta <nlsima97@gmail.com>
2024-12-26 20:53:15 -0600

    projects where people try to enter a URL are also handled

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-10rem)]  text-center">
+    <div className="flex flex-col items-center  min-h-[calc(100vh-10rem)]  text-center">
       <h1 className="text-6xl font-bold text-gray-800 mb-4">404</h1>
       <h2 className="text-2xl text-gray-600 mb-2">Page Not Found</h2>
       <p className="text-gray-500 mb-6">
--- src/services/projects/project.ts
+++ src/services/projects/project.ts
@@ -1,5 +1,6 @@
 // Fetch project data from Supabase
 import supabase from '@/src/lib/supabaseClient';
+import { notFound } from 'next/navigation';
 // import { Project as ProjectType } from '@/src/types';
 
 export async function getProject(projectID: string) {
@@ -15,6 +16,10 @@ export async function getProject(projectID: string) {
     .eq('id', projectID)
     .single();
 
+  // If data is null, send them to the 404 not found
+  if (!data) {
+    notFound();
+  }
   if (error) {
     console.error('Error fetching project data:', error.message);
     throw error;
