72d5d1836af09bba5d914fbff7eb2db61bab413c
Neal Matta <nlsima97@gmail.com>
2025-01-20 13:50:31 -0600

    Small change to not found to make it more in line with the primary buttons

--- src/components/reusable/UI/PrimaryButton.tsx
+++ src/components/reusable/UI/PrimaryButton.tsx
@@ -5,17 +5,20 @@ interface ButtonProps {
   linkTo: string;
   children: ReactNode;
   className?: string; // Optional with a default value
+  target?: string;
 }
 
 export default function Button({
   linkTo,
   className = '',
+  target = '_blank',
+
   children,
 }: ButtonProps) {
   return (
     <Link
       href={linkTo}
-      target="_blank"
+      target={target}
       className={`bg-primary hover:bg-primary-dark focus:bg-primary-light text-white font-bold py-2 px-4 rounded-full ${className}`}
     >
       {children}
