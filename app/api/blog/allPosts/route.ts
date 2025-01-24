b3380960ce541e6ce96a3f5096665736fb619c26
Neal Matta <nlsima97@gmail.com>
2025-01-23 22:51:32 -0600

    Just read the documentation to figure out how to pull the data properly

--- app/api/blog/allPosts/route.ts
+++ app/api/blog/allPosts/route.ts
@@ -3,9 +3,11 @@ import { Client } from '@notionhq/client';
 export async function GET() {
   const notion = new Client({ auth: process.env.NOTION_SECRET });
 
-  const data = await notion.blocks.children.list({
-    block_id: process.env.NOTION_BLOG_PAGE_ID!,
+  const response = await notion.databases.query({
+    database_id: process.env.NOTION_BLOG_PAGE_ID!,
   });
+
+  const data = response.results;
 
   return new Response(JSON.stringify(data), {
     status: 200,
