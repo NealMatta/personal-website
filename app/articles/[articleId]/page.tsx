88b36a9dee175ecd6164b07f881e923f35a98806
Neal Matta <nlsima97@gmail.com>
2025-01-26 23:16:07 -0600

    Used suspense and component restructuring to get the code i wanted loaded to load. Need to continue to iterate on this idea

--- app/articles/[articleId]/page.tsx
+++ app/articles/[articleId]/page.tsx
@@ -1,18 +1,14 @@
+import React, { Suspense } from 'react';
 import style from '@/src/styles/article.module.css';
 import ReactMarkdown from 'react-markdown';
-
 import { getArticle } from '@/src/apiManagement/articles/articles';
-export default async function IndividualArticle({
-  params,
-}: {
-  params: Promise<{ articleId: string }>;
-}) {
-  const articleID = (await params).articleId;
-  const post = await getArticle(articleID);
 
+// Component to fetch article data
+const ArticleContent = async ({ articleID }: { articleID: string }) => {
+  const post = await getArticle(articleID);
   return (
-    <section className="flex flex-col gap-y-4">
-      <div className="">
+    <>
+      <div>
         <h1 className="text-4xl font-bold">{post.metadata.title}</h1>
         <div className="flex space-x-2">
           <p>{post.metadata.date}</p>
@@ -21,10 +17,27 @@ export default async function IndividualArticle({
         </div>
       </div>
       <hr />
-
       <div className={style.notion}>
         <ReactMarkdown>{post.markdown.parent}</ReactMarkdown>
       </div>
+    </>
+  );
+};
+
+// Main component
+export default async function IndividualArticle({
+  params,
+}: {
+  params: { articleId: string };
+}) {
+  const articleId = (await params).articleId;
+  return (
+    <section className="flex flex-col gap-y-4">
+      <Suspense
+        fallback={<div className="text-center">Loading article...</div>}
+      >
+        <ArticleContent articleID={articleId} />
+      </Suspense>
     </section>
   );
 }
